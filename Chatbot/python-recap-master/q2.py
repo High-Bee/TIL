979a6c4b0e277090c6b3e66c1f7bcac43366e036
High-Bee <ko.yb112@gmail.com>
2020-01-10 23:09:10 +0900

    markdown add

--- Chatbot/python-recap-master/q2.py
+++ Chatbot/python-recap-master/q2.py
@@ -1,9 +1,11 @@
-'''
-문제 2.
-자연수 N이 주어졌을 때, 1부터 N까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
-'''
+# '''
+# 문제 2.
+# 자연수 N이 주어졌을 때, 1부터 N까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+# '''
 
-numbers = int(input('숫자를 입력하세요: '))
+# numbers = int(input('숫자를 입력하세요: '))
 
-for i in range(1,numbers+1):
-    print(i)
+# for i in range(1,numbers+1):
+#     print(i)
+x = [30,10,10]
+print(x.count(30))
